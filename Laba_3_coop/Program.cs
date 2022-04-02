fa27e0f96c6827aeebfe7b83cde435e81de92de5
gusus21 <yanenko.yurii1121@vu.cdu.edu.ua>
2022-04-02 18:20:55 +0300

    Start of the project: prototype of menu

---
+++ Laba_3_coop/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Laba_3_coop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 2, what variant are you want: 10, 12 or 17?\nTo exit enter '0'");
+            int variant;
+            do
+            {
+                variant = int.Parse(Console.ReadLine());
+                switch (variant)
+                {
+                    case 10:
+                        {
+                            break;
+                        }
+                    case 12:
+                        {
+                            break;
+                        }
+                    case 17:
+                        {
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Choose variant please or enter 0 to exit");
+                            break;
+                        }
+                }
+            } while (variant != 0);
+        }
+    }
+}
